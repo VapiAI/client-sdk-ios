3de0c2bb67b25082bb33b1dfcc7751eca941d811
Anton Begehr <anton@begehr.me>
2025-07-10 18:40:10 +0400

    error descriptions

--- Sources/Models/VapiError.swift
+++ Sources/Models/VapiError.swift
@@ -7,11 +7,30 @@
 
 import Foundation
 
-public enum VapiError: Swift.Error {
+public enum VapiError: LocalizedError {
     case invalidURL
     case customError(String)
     case existingCallInProgress
     case noCallInProgress
     case decodingError(message: String, response: String? = nil)
     case invalidJsonData
+}
+
+extension VapiError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "URL is invalid"
+        case .customError(let message):
+            return message
+        case .existingCallInProgress:
+            return "An existing call is in progress"
+        case .noCallInProgress:
+            return "No call in progress"
+        case .decodingError(let message, let response):
+            return "\(message)\n\(response ?? "No response data")"
+        case .invalidJsonData:
+            return "Invalid JSON data"
+        }
+    }
 }
